8f56b00ad1ee45736033130e7e8bde935f2b5720
Madhura <bamadhura13@gmail.com>
2023-12-21 13:01:17 +0530

    2nd project intialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>2nd project</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel blanditiis, omnis praesentium, quis magni, ad architecto molestiae numquam autem eveniet quasi sapiente vitae culpa. Sint mollitia harum doloremque non provident.</p>
+    </>
+  )
+}
+
+export default App
